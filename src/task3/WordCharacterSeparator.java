bfd952b77290ece38ad829cc55b88b0b7d482133
Jaroslav Jermolovitsh <jaroslav0911@gmail.com>
2019-04-28 10:57:45 +0300

    Task3

--- src/task3/WordCharacterSeparator.java
+++ src/task3/WordCharacterSeparator.java
@@ -1,4 +1,13 @@
 package task3;
 
+import java.util.Scanner;
+
 public class WordCharacterSeparator {
+    public static void main(String[] args) {
+        System.out.println("Welcome stranger. Enter String");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        name = name.replace("", " ").trim();
+        System.out.println(name);
+    }
 }
