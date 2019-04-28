8de592f86937ae4f4e0ea222edbfe5c37bafb2a3
Jaroslav Jermolovitsh <jaroslav0911@gmail.com>
2019-04-28 09:03:55 +0300

    2 commit

--- src/recap/Welcome.java
+++ src/recap/Welcome.java
@@ -7,10 +7,9 @@ public class Welcome {
         System.out.println("Welcome stranger. Please enter Your full name");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
-        String nameTrue = "James Bond";
-        String name2 = "james bond";
+        String nameTrue = "james bond";
 
-        if (name.toLowerCase().equals(name2)){
+        if (name.toLowerCase().equals(nameTrue)){
             System.out.println("Welcome 007");
         } else {
             System.out.println("You are not agent. Get out!");
